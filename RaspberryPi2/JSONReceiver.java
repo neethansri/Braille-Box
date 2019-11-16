429873202012a590062e808b1c6c9c74f11fd1d0
Idir Zerrouk <idirzerrouk11@gmail.com>
2019-11-15 21:11:00 -0500

    JSON

---
+++ RaspberryPi2/JSONReceiver.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+
+public class JSONReceiver {
+
+    public static void main(String[] args) throws IOException {
+    	DatagramSocket socket1 = new DatagramSocket(1000);
+    	while(true) {
+    		System.out.println( "Receiving on port " + 1000 ) ;
+    		DatagramPacket packet = new DatagramPacket(new byte[256], 256);
+    		socket1.receive( packet ) ;
+    		System.out.println( packet.getAddress() + " " +
+    		packet.getPort() + ": " +
+    		new String(packet.getData()).trim());
+    		}
+    }
+    
+    
+}
