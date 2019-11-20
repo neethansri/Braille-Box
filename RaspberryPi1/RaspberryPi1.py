fe52a7658985f7d5eb582339b55c0846a902465a
Neethan Sriranganathan <neethan.sriranganathan@gmail.com>
2019-11-19 22:05:00 -0500

    Finishing testing code for the database

--- RaspberryPi1/RaspberryPi1.py
+++ RaspberryPi1/RaspberryPi1.py
@@ -1,7 +1,9 @@
 import sqlite3
+import json
 import socket, sys, time
 
 class DB():
+    @staticmethod
     def create_table(conn):
         """create tables for the database 
         :param conn: Connection object
@@ -9,7 +11,7 @@ class DB():
         """
         
          # Creates the message_Info table with the following columns"""
-        sql = ''' CREATE TABLE message_Info (
+        sql = ''' CREATE TABLE IF NOT EXISTS message_Info (
                     message text NOT NULL,
                     messageSent boolean NOT NULL,
                     messageId integer NOT NULL PRIMARY KEY
@@ -64,6 +66,7 @@ class DB():
         curs = connection.cursor()
         #print ("\nEntire Database contents:\n")
         curs.execute("SELECT * FROM message_Info WHERE messageSent = '0'")
+        connection.commit()
         rows = curs.fetchall()
         return rows
 
@@ -76,29 +79,45 @@ class DB():
         # sql statements to be executed to find max value in messageId column"""
         sql = ''' SELECT MAX(messageId) FROM message_Info  '''
         cur = connection.cursor()
-        connection.commit()
         cur.execute(sql)
-              
+        connection.commit()
+        
         idValue = cur.fetchone()[0]
-        idValue = idValue + 1
+        if(idValue == None):
+            idvValue = 0
+        else:
+            idValue = idValue + 1
         return idValue
-    
-
-
+    @staticmethod
+    def parsejson():
+        with open('data.json','r')as f:
+            dataVals = json.load(f)
+            
+        for val in dataVals:
+            message = val['message']
+            Sent = val['messageSent']
+            msgid = val['messageId']
+            packet = (message,Sent,msgid)
+        return packet
+        
+    @staticmethod
     def addMessageInfo(tableValue):
         """Add data to the columns in the message_Info table
         :param conn: Connection object
         :param tableValue: data to be put into the database
         :return: the last row value
         """
-        connection = server()
+        connection = DB.server()
         sql = ''' INSERT INTO message_Info(message,messageSent,messageId)
 
                 VALUES(?,?,?) '''
         cur = connection.cursor()
         cur.execute(sql, tableValue)
         connection.commit()
-        return cur.lastrowid  # used for connecting tables
+        connection.close()
+        return "ran"
+        #connection.commit()
+        #return cur.lastrowid  # used for connecting tables
 
 
     def addMessageContainer(tableValue):
@@ -130,7 +149,7 @@ class DB():
         conn.commit()
         return cur.lastrowid  # used for connecting tables
 
-
+    @staticmethod
     def updateSentStatus(connection, idValue):
         """Update boolean flag for specific messages in the message_Info table
         :param conn: Connection object
@@ -144,6 +163,23 @@ class DB():
         cur = connection.cursor()
         cur.execute(sql, (idValue,))
         connection.commit()
+        
+    @staticmethod
+    def getSentList(conn):
+        
+        curs = conn.cursor()
+        curs.execute("SELECT * FROM message_Info WHERE messageSent = '1'")
+        rows = curs.fetchall()
+        return rows
+    
+    @staticmethod
+    def getAllMessages(conn):   
+        curs = conn.cursor()
+        sql = (''' SELECT*FROM message_Info ''')
+        
+        curs.execute(sql)
+        rows = curs.fetchall()
+        return rows
 
 
     def getLengthOfMessage(connection, idValue):
@@ -161,8 +197,14 @@ class DB():
         strings = cur.fetchone()[0]
         length = len(strings)
         print(length)
-
-
+    
+    @staticmethod
+    def deleteTable(conn):
+        sql = ''' DELETE FROM message_Info '''
+        cur = conn.cursor()
+        cur.execute(sql)
+        conn.commit()
+        
     #Varaiables used for communication part of the code"""
     port = 1001
     hostAddress = ''
@@ -190,7 +232,7 @@ class DB():
         print(hostAddress)
         data = str(result)
         s.sendto(data.encode('utf-8'), hostAddress)
-
+    @staticmethod
     def parseText(message):
         message = message.lower()
         formattedArray = list()
@@ -216,5 +258,19 @@ class DB():
             
         
         return formattedArray
+
+#def main():
+        f#oo = DB()
+        #test = foo.parseText('test123test')
+        #print(test)
+        #conn = foo.server()
+        #p = ('test',0,4)
+        #foo.addMessageInfo(p)
+       # foo.deleteTable(conn)
+        
+#if __name__=='__main__':
+  #  main()
+
+
 
 
--- RaspberryPi1/TestDbCall.py
+++ RaspberryPi1/TestDbCall.py
@@ -1,37 +1,130 @@
 
 import RaspberryPi1
 import unittest
+import json
+import sqlite3
 
 
 
 class TestDatabaseCommands(unittest.TestCase):
-   # testdb = RaspberryPi1.DB()
     
-    def test_getNotSent(self):
         
-        testdb = RaspberryPi1.DB()
-        
-        conn = testdb.server()
-        p = testdb.getNotSent(conn)
-        r=[('test5',0,12)]
-        self.assertEqual(p,r)
-    
     def test_insertMessage(self):
+        try:
+            testdb = RaspberryPi1.DB()
+            
+            conn = testdb.server()
+            cur = conn.cursor()
+            testdb.create_table(conn)
+            
+            p = testdb.parsejson()
+            print(p)
+            testdb.addMessageInfo(p)
+            
+            r = testdb.getAllMessages(conn)
+            print(r)
+            q = [("jsonTest",0,5)]
+            self.assertEqual(r,q)
+            
+        except sqlite3.Error as error:
+            
+             print("Failed to insert record from sqlite table",error)
+        finally:
+            if(conn):
+                testdb.deleteTable(conn)
+                conn.close()
+                print("\nConnection Close")
         
-        testdb = RaspberryPi1.db()
-        
-        conn = testdb.server()
-        p = testdb.addMessageInfo()
-  
-        
+    def test_getNotSent(self):
+        try:
+            testdb = RaspberryPi1.DB()
+            
+            conn = testdb.server()
+            cur = conn.cursor()
+            testdb.create_table(conn)
+            testdb.addMessageInfo(('test',0,0))
+            p= testdb.getNotSent(conn)
+            print(p)
+            r = [("test",0,0)]
+            
+            self.assertEqual(p,r)
+            
+        except sqlite3.Error as error:
+                
+                print("Error: ",error)
+        finally:
+            if(conn):
+                testdb.deleteTable(conn)
+                conn.close()
+                print("\nConnection Close")
+                
     
-    def main():
-        test_getNotSent()
-         
-#if__name__=='__main__'
-unittest.main()
+    def test_UpdateSentStatus(self):
+        try:
+            testdb = RaspberryPi1.DB()
+            
+            conn = testdb.server()
+            cur = conn.cursor()
+            testdb.create_table(conn)
+            testdb.addMessageInfo(('test',0,0))
+            
+            testdb.create_table(conn)
+            testdb.addMessageInfo(('test',0,1))
+            
+            r = testdb.getAllMessages(conn)
+            print(r)
+            
+            testdb.updateSentStatus(conn,0)
+            
+            p = testdb.getSentList(conn)
+            print(p)
+            
+            a = testdb.getNotSent(conn)
+            print(a)
+            
+            self.assertEqual(p,([('test',1,0)]))
+            
+        except sqlite3.Error as error:
+                print("Error :",error)
+        finally:
+            if(conn):
+                testdb.deleteTable(conn)
+                conn.close()
+                print("\nConnection Close")
+                
         
         
-        
+       
+                    
+    def test_parseText(self):
+        try:
+            testdb = RaspberryPi1.DB()
+            
+            val = testdb.parseText('test')
+            print(val)
+            
+            val1 = testdb.parseText('123')
+            print(val1)
+            
+            val2 = testdb.parseText('test123')
+            print(val2)
+            
+            val3 = testdb.parseText('test123test')
+            print(val3)
+            
+            self.assertEqual(val,(['t', 'e', 's', 't']))
+            self.assertEqual(val1,(['#', 'a', 'b', 'c']))
+            self.assertEqual(val2,(['t', 'e', 's', 't', '#', 'a', 'b', 'c']))
+            self.assertEqual(val3,(['t', 'e', 's', 't', '#', 'a', 'b', 'c', ';', 't', 'e', 's', 't']))
+            
+            
+        except sqlite3.Error as error:
+            print("Error :",error)
+        finally:
+                print("\n Conversion Done")
+            
+   
+if __name__ == '__main__':
+    unittest.main()        
 
 
