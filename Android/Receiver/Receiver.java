1d66f1fdf81ec8ea15175819f98b566c851969f5
moeissa <mohdnedal.issa@gmail.com>
2019-11-23 16:16:00 -0500

    updated app with JSON (no repeated messages)

--- Android/Receiver/Receiver.java
+++ Android/Receiver/Receiver.java
@@ -16,7 +16,8 @@ public class Receiver {
 	static ServerSocket serverSocket;
 	static InputStreamReader inputStreamReader;
 	static BufferedReader bufferReader;
-	static String message;
+	static String newMessage;
+	static String oldMessage;
 	
 
 	public static void main(String[] args) {
@@ -25,44 +26,52 @@ public class Receiver {
 			serverSocket = new ServerSocket(1001);
 			
 			int count = 0;
-			PrintStream history = new PrintStream(new File("C:\\Users\\mohd-\\Desktop\\history.txt"));
-
+			PrintStream history = new PrintStream(new File("E:\\Users\\Moe\\Desktop\\history.txt"));
+			FileWriter fileWriter = new FileWriter("E:\\Users\\Moe\\Desktop\\JSONBox.JSON");
+			oldMessage = "";
 			while(true) {
 				
 				socket = serverSocket.accept();
 				inputStreamReader = new InputStreamReader(socket.getInputStream());
 				bufferReader = new BufferedReader(inputStreamReader);
-				message = bufferReader.readLine();
-				System.out.println("Receiving characters");
-				count++;
-				PrintStream pi = new PrintStream(new File("C:\\Users\\mohd-\\Desktop\\pi.txt"));
-				// Creating a File object that represents the disk file.  
-		        PrintStream console = System.out; 
-		        
-		        // check if the same person sends the same message
-		        String list[] = message.split(",");
-		        JSONObject obj = new JSONObject();
-		        for (String i : list){
-		        	String temp[] = i.split(":");
-		        	obj.put(temp[0], temp[1]);
-		        }
-		        FileWriter fileWriter = new FileWriter("C:\\Users\\mohd-\\Desktop\\JSONBox.JSON", true); //Set true for append mode
-		        PrintWriter pw = new PrintWriter(fileWriter);
-		        pw.write(obj.toJSONString()+"\n"); 
-		        pw.flush();
-		        pw.close();
-		        
-		        // Assign pi to output stream 
-		        System.setOut(pi);
-		        System.out.println(message);
-		        
-		        // Assign history to output stream 
-		        System.setOut(history); 
-		        System.out.println("["+count+"]"+message); 
-		  
-		        // Use stored value for output stream 
-		        System.setOut(console); 
-		        System.out.println("["+count+"]"+message);
+				newMessage = bufferReader.readLine();
+				if (!(oldMessage.equals(newMessage))) {
+					System.out.println("Receiving characters");
+					count++;
+					PrintStream pi = new PrintStream(new File("E:\\Users\\Moe\\Desktop\\pi.txt"));
+					// Creating a File object that represents the disk file.  
+			        PrintStream console = System.out; 
+			        
+			        // check if the same person sends the same message
+			        String list[] = newMessage.split(",");
+			        JSONObject obj = new JSONObject();
+			        for (String i : list){
+			        	String temp[] = i.split(":");
+			        	obj.put(temp[0], temp[1]);
+			        }
+			        fileWriter = new FileWriter("E:\\Users\\Moe\\Desktop\\JSONBox.JSON", true); //Set true for append mode
+			        PrintWriter pw = new PrintWriter(fileWriter);
+			        pw.write(obj.toJSONString()+"\n"); 
+			        pw.flush();
+			        pw.close();
+			        
+			        // Assign pi to output stream 
+			        System.setOut(pi);
+			        System.out.println(newMessage);
+			        
+			        // Assign history to output stream 
+			        System.setOut(history); 
+			        System.out.println("["+count+"]"+newMessage); 
+			  
+			        // Use stored value for output stream 
+			        System.setOut(console); 
+			        System.out.println("["+count+"]"+newMessage);
+			        
+			        oldMessage = newMessage;
+				}else {
+					System.out.println("Message was already sent!");
+				}
+
 		        
 
 			}
