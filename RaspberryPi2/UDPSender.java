3130d23d53b6dbf03146e369b1a7bd3fcbd06737
Idir Zerrouk <idirzerrouk11@gmail.com>
2019-12-06 23:50:04 -0500

    Create UDPSender.java

---
+++ RaspberryPi2/UDPSender.java
@@ -0,0 +1,36 @@
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.Scanner;
+
+/**
+ * Basic UDP sender to test communication
+ * @author idirz
+ *
+ */
+public class JSONSender extends Thread {
+	
+	public static void main(String[] args) throws IOException {
+		
+		Scanner scanner = new Scanner(System.in);
+		DatagramSocket socket1 = new DatagramSocket();
+		while (true) {
+			
+			System.out.println("Enter text to be sent, ENTER to quit ");
+			String message = scanner.nextLine();
+			if (message.length()==0) break;
+			byte [] data = message.getBytes() ;
+			
+			DatagramPacket packet = new DatagramPacket(data, data.length, InetAddress.getByName("10.0.0.1"), Integer.parseInt(args[0]) ) ;
+			socket1.send( packet );
+			
+
+		}
+		scanner.close();
+		socket1.close();
+	}
+	
+}
